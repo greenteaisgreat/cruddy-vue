7dba0a6dfc1df392260dabe38f43cda3bc5a02bd
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-06-03 16:54:57 -0500

    Add TodoHeader Component

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,20 @@
+<script setup>
+  import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+  <header>
+    <nav class="container">
+      <div class="branding">
+        <img src="../assets/Vue_Logo_Black.png" alt="Vue Logo" />
+        <h1>Vue To-Do's</h1>
+      </div>
+      <ul class="nav-routes">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style lang="scss" scoped></style>
