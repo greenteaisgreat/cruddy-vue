790428b81ed56510555d3806d0655357413527b9
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2024-06-03 17:11:10 -0500

    Add Styling for TodoHeader Component

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -7,7 +7,7 @@
     <nav class="container">
       <div class="branding">
         <img src="../assets/Vue_Logo_Black.png" alt="Vue Logo" />
-        <h1>Vue To-Do's</h1>
+        <h1>Cruddy Vues</h1>
       </div>
       <ul class="nav-routes">
         <RouterLink to="/">Home</RouterLink>
@@ -17,4 +17,39 @@
   </header>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  header {
+    background-color: #536376;
+    nav {
+      display: flex;
+      align-items: center;
+      padding: 25px 16px;
+
+      .branding {
+        display: flex;
+        align-items: center;
+        gap: 8px;
+        img {
+          max-width: 50px;
+        }
+
+        h1 {
+          font-size: 24px;
+        }
+      }
+
+      .nav-routes {
+        display: flex;
+        flex: 1;
+        justify-content: flex-end;
+        gap: 12px;
+        list-style: none;
+
+        a {
+          text-decoration: none;
+          color: inherit;
+        }
+      }
+    }
+  }
+</style>
